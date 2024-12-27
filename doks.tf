d3e8768114dbe55f1ae242aa22465be523ec645a
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-12-27 16:21:10 -0700

    test plan

--- doks.tf
+++ doks.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "doks-drewpy" {
   name   = "doks-drewpy"
   region = "nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.31.1-do.5"
+  version = "1.22.8-do.1"
 
   node_pool {
     name       = "worker-pool"
@@ -13,7 +13,7 @@ resource "digitalocean_kubernetes_cluster" "doks-drewpy" {
     start_time = "04:00"
     day        = "sunday"
   }
-  
+
     taint {
       key    = "workloadKind"
       value  = "application"
