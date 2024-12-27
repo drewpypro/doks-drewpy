a0ecc7fc2aa767a8ea0ad2dde52ea2caa60e6899
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-12-27 12:15:41 -0700

    test plan

--- providers.tf
+++ providers.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
+  }
 }
 
 provider "digitalocean" {
--- variables.tf
+++ variables.tf
@@ -3,25 +3,25 @@ variable "DIGITALOCEAN_TOKEN" {
   type        = string
 }
 
-variable "region" {
-  description = "Region where the resources will be created"
-  type        = string
-  default     = "nyc1"
-}
+# variable "region" {
+#   description = "Region where the resources will be created"
+#   type        = string
+#   default     = "nyc1"
+# }
 
-variable "size" {
-  description = "size of the resources that will be created (https://slugs.do-api.dev/)"
-  type        = string
-  default     = "s-1vcpu-2gb"
-}
+# variable "size" {
+#   description = "size of the resources that will be created (https://slugs.do-api.dev/)"
+#   type        = string
+#   default     = "s-1vcpu-2gb"
+# }
 
-variable "source_ssh_networks" {
-  description = "Source Public IP addresses of your ssh client"
-  type        = list(string)
-}
+# variable "source_ssh_networks" {
+#   description = "Source Public IP addresses of your ssh client"
+#   type        = list(string)
+# }
 
-variable "game_networks" {
-  description = "Source Public IP addresses of your ssh client"
-  type        = list(string)
-}
+# variable "game_networks" {
+#   description = "Source Public IP addresses of your ssh client"
+#   type        = list(string)
+# }
 
